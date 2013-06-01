9c242dbfb637adca9550ed0f695800f97381617f
Indraniel Das <indraniel@gmail.com>
2013-06-01 12:43:18 -0500

    + improve the env setup notifications
      - colorize the output
      - update the report to include all env variables set
      - change report structure a bit for easier reading

--- setup-go-env.sh
+++ setup-go-env.sh
@@ -1,5 +1,18 @@
 # rudimentary golang setup
 
+function warn() {
+    printf '\033[0;31m%s\033[0m\n' "$1" >&2
+}
+
+function die() {
+    warn "$1"
+    exit 1
+}
+
+function note() {
+    printf '\033[0;34m%s\033[0m\n' "$1"
+}
+
 export PATH=$HOME/golang/bin:$PATH
 export GOPATH=$HOME/gopkgs         # place where we develop and store external pkgs
 export GOROOT=$HOME/golang
@@ -16,11 +29,20 @@ x86_64)
     ;;
 esac
 
-echo "GOBIN : $GOBIN"
-echo "PATH  : $PATH"
-echo "GOROOT: $GOROOT"
-echo "GOARCH: $GOARCH"
 if [ ! -d "$GOPATH" ]; then
     note "---> GOPATH directory '$GOPATH' does not exist - Creating directory"
     mkdir -p $GOPATH
 fi
+
+note "GOROOT: "
+echo -e "\t$GOROOT"
+note "GOBIN: "
+echo -e "\t$GOBIN"
+note "GOPATH: "
+echo -e "\t$GOPATH"
+note "GOOS"
+echo -e "\t$GOOS"
+note "GOARCH: "
+echo -e "\t$GOARCH"
+note "PATH: "
+echo -e "\t$PATH"
