11d892e8477e69a29f01b5447803b0f67b845d29
Indraniel Das <indraniel@gmail.com>
2016-10-15 19:00:00 -0500

    + make helm mode buffer more vim-like
      - be able to use "j", "k" to move up and down lines
      - use "q" to quit the buffer/mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -73,6 +73,9 @@ re-downloaded in order to locate PACKAGE."
 ; helm setup
 (require 'helm-config)
 (helm-mode 1)
+(define-key helm-map (kbd "j") 'helm-next-line)
+(define-key helm-map (kbd "k") 'helm-previous-line)
+(define-key helm-map (kbd "q") 'helm-quit)
 
 ;; company / auto-completion setup
 (defun my/python-mode-hook ()
