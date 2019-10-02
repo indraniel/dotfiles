31b489758a72e3fb0b4d608952a23f24cfc3e99f
Indraniel Das <indraniel@gmail.com>
2019-10-01 22:21:44 -0500

    + add lein-try

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,4 @@
 {:user {:plugins [[cider/cider-nrepl "0.21.0"]
-                  [venantius/ultra "0.6.0"]]
+                  [venantius/ultra "0.6.0"]
+                  [lein-try "0.4.3"]]
         :dependencies [[cljfmt "0.6.4"]]}}
