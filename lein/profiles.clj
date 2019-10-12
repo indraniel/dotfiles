835cd521423c0420b092fb386cc1668a7f6ed5d4
indraniel <indraniel@gmail.com>
2019-10-12 11:14:46 -0500

    + update to latest cider-nrepl

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,4 @@
-{:user {:plugins [[cider/cider-nrepl "0.21.0"]
+{:user {:plugins [[cider/cider-nrepl "0.22.4"]
                   [venantius/ultra "0.6.0"]
                   [lein-try "0.4.3"]]
         :dependencies [[cljfmt "0.6.4"]]}}
