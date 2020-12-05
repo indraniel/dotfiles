cd969f48e0b2ab7d0ef3a894c1d2ef6e55f6b14b
indraniel <indraniel@gmail.com>
2020-12-05 13:24:08 -0600

    + update the lein profile
      - include the ability to start a rebel readline repl

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,6 @@
 {:user {:plugins [[cider/cider-nrepl "0.22.4"]
                   [venantius/ultra "0.6.0"]
                   [lein-try "0.4.3"]]
-        :dependencies [[cljfmt "0.6.4"]]}}
+        :dependencies [[cljfmt "0.6.4"]
+                       [com.bhauman/rebel-readline "0.1.4"]]
+        :aliases  {"rebl" ["trampoline" "run" "-m" "rebel-readline.main"]}}
