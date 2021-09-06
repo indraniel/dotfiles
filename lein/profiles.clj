3a70102556f5d6aac216ef098b324342d5b07c7e
indraniel <indraniel@gmail.com>
2021-09-06 14:46:53 -0500

    + account for rebel readline with lein

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -3,4 +3,4 @@
                   [lein-try "0.4.3"]]
         :dependencies [[cljfmt "0.6.4"]
                        [com.bhauman/rebel-readline "0.1.4"]]
-        :aliases  {"rebl" ["trampoline" "run" "-m" "rebel-readline.main"]}}
+        :aliases  {"rebl" ["trampoline" "run" "-m" "rebel-readline.main"]}}}
