fc9cd2b5e393ca518d6bea2bb0e249b9ebde1d0d
indraniel <indraniel@gmail.com>
2023-05-29 10:58:36 -0500

    + indention cleanup
      - more standard look

--- nvim2/lua/indraniel/packer.lua
+++ nvim2/lua/indraniel/packer.lua
@@ -65,14 +65,14 @@ return require('packer').startup(function(use)
             run = function()
                 pcall(vim.cmd, 'MasonUpdate')
             end,
-        },
-        {'williamboman/mason-lspconfig.nvim'}, -- Optional
+            },
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-        -- Autocompletion
-        {'hrsh7th/nvim-cmp'},     -- Required
-        {'hrsh7th/cmp-nvim-lsp'}, -- Required
-        {'L3MON4D3/LuaSnip'},     -- Required
-    }
-})
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},     -- Required
+            {'hrsh7th/cmp-nvim-lsp'}, -- Required
+            {'L3MON4D3/LuaSnip'},     -- Required
+        }
+    })
 
 end)
