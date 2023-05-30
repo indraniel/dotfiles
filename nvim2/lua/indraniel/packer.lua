3447eae337009bb313db90998232f1cd6a78fbdf
indraniel <indraniel@gmail.com>
2023-05-30 00:59:12 -0500

    + remove extra whitespace

--- nvim2/lua/indraniel/packer.lua
+++ nvim2/lua/indraniel/packer.lua
@@ -29,7 +29,7 @@ return require('packer').startup(function(use)
     use({
         "nvim-neo-tree/neo-tree.nvim",
         branch = "v2.x",
-        requires = { 
+        requires = {
             "nvim-lua/plenary.nvim",
             "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
             "MunifTanjim/nui.nvim",
