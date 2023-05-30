c6baa13a590a2374aca35be1eb011389e165f9a8
indraniel <indraniel@gmail.com>
2023-05-30 00:59:52 -0500

    + intiailize metals as a function
      - we only want packer to load this module optionally, when we are
        working on scala files.
      - i think there's a issue with the metals package, but not sure
        exactly what yet.  This approach deals with the issue for now...

--- nvim2/after/plugin/metals.lua
+++ nvim2/after/plugin/metals.lua
@@ -1,54 +1,55 @@
-local api = vim.api
--- local cmd = vim.cmd
--- local map = vim.keymap.set
+-- --vim.cmd([[autocmd FileType scala,sbt lua require("metals").initialize_or_attach({})]])
+function InitMetals()
+    local metals = require("metals")
 
-local metals_config = require("metals").bare_config()
+    local metals_config = metals.bare_config()
 
- -- Example of settings
- metals_config.settings = {
-     showImplicitArguments = true,
-     excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
- }
- metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
+     -- Example of settings
+     metals_config.settings = {
+         showImplicitArguments = true,
+         excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+     }
+     metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
 
- -- Debug settings if you're using nvim-dap
- local dap = require("dap")
+     -- Debug settings if you're using nvim-dap
+     local dap = require("dap")
 
- dap.configurations.scala = {
-     {
-         type = "scala",
-         request = "launch",
-         name = "RunOrTest",
-         metals = {
-             runType = "runOrTestFile",
-             --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+     dap.configurations.scala = {
+         {
+             type = "scala",
+             request = "launch",
+             name = "RunOrTest",
+             metals = {
+                 runType = "runOrTestFile",
+                 --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+             },
          },
-     },
-     {
-         type = "scala",
-         request = "launch",
-         name = "Test Target",
-         metals = {
-             runType = "testTarget",
+         {
+             type = "scala",
+             request = "launch",
+             name = "Test Target",
+             metals = {
+                 runType = "testTarget",
+             },
          },
-     },
- }
+     }
 
- metals_config.on_attach = function(client, bufnr)
-     require("metals").setup_dap()
- end
+     metals_config.on_attach = function(client, bufnr)
+         metals.setup_dap()
+     end
 
- -- Autocmd that will actually be in charging of starting the whole thing
- local nvim_metals_group = api.nvim_create_augroup("nvim-metals", { clear = true })
- api.nvim_create_autocmd("FileType", {
-     -- NOTE: You may or may not want java included here. You will need it if you
-     -- want basic Java support but it may also conflict if you are using
-     -- something like nvim-jdtls which also works on a java filetype autocmd.
-     pattern = { "scala", "sbt" },
-     callback = function()
-         require("metals").initialize_or_attach(metals_config)
-     end,
-     group = nvim_metals_group,
- })
+--     -- Autocmd that will actually be in charging of starting the whole thing
+--     local nvim_metals_group = vim.api.nvim_create_augroup("nvim-metals", { clear = true })
+--     vim.api.nvim_create_autocmd("FileType", {
+--         -- NOTE: You may or may not want java included here. You will need it if you
+--         -- want basic Java support but it may also conflict if you are using
+--         -- something like nvim-jdtls which also works on a java filetype autocmd.
+--         pattern = { "scala", "sbt" },
+--         callback = function()
+--             metals.initialize_or_attach(metals_config)
+--         end,
+--         group = nvim_metals_group,
+--     })
 
--- vim.cmd([[autocmd FileType scala,sbt lua require("metals").initialize_or_attach({})]])
+     metals.initialize_or_attach(metals_config)
+end
